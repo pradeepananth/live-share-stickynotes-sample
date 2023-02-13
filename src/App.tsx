b7ec6548fad41d5032982fcdfae28c8e87bb6329
Pradeep Anantharaman <pradan@microsoft.com>
2023-02-12 20:19:29 -0800

    Add note functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Note } from "./Note";
+import "./App.css";
 
 function App() {
+  // state for storing notes
+  // state for the notes array
+  const [notes, setNotes] = useState([]);
+
+  // state for the input value
+  const [input, setInput] = useState("");
+
+  // function to handle input change
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  // function to handle form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // create a new note object with the input value and a random color
+    const newNote = {
+      text: input,
+      color: `hsl(${Math.floor(Math.random() * 360)}, 100%, 75%)`,
+    };
+    // update the notes array with the new note
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+    // clear the input value
+    setInput("");
+  };
+
+  // function to handle note delete
+  const handleDelete = (index) => {
+    // filter out the note at the given index
+    setNotes((prevNotes) => prevNotes.filter((note, i) => i !== index));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sticky Notes</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="text-box"
+          type="text"
+          value={input}
+          onChange={handleChange}
+          placeholder="Write a note..."
+          required
+        />
+        <button className="button" type="submit">
+          Add
+        </button>
+      </form>
+      <div className="notes-container">
+        {notes.map((note, index) => (
+          // render a Note component for each note in the array
+          <Note
+            key={index}
+            index={index}
+            text={note.text}
+            color={note.color}
+            onDelete={handleDelete}
+          />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/Note.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./note.css";
+export const Note = ({ index, text, color, onDelete }) => {
+  // function to handle delete button click
+  const handleClick = () => {
+    // call the onDelete prop with the index of the note
+    onDelete(index);
+  };
+
+  return (
+    // render a div with the note class and the given color
+    <div className="note" style={{ backgroundColor: color }}>
+      {/* render the text prop as the note content */}
+      <p>{text}</p>
+      {/* render a button with the delete-button class and the handleClick function */}
+      <button className="delete-button" onClick={handleClick}></button>
+    </div>
+  );
+};
+
+export default Note;
