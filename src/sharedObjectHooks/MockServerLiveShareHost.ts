ea7989276fb697d3f9848cfedfd4637d6d4f819e
Pradeep Anantharaman <pradan@microsoft.com>
2023-05-09 15:00:29 -0700

    Removing Axios and moving to fetch API

--- src/sharedObjectHooks/MockServerLiveShareHost.ts
+++ src/sharedObjectHooks/MockServerLiveShareHost.ts
@@ -6,9 +6,7 @@ import {
   INtpTimeInfo,
   UserMeetingRole,
 } from "@microsoft/live-share";
-import { AxiosInstance, CreateAxiosDefaults, default as axios } from "axios";
-
-const LiveShareRoutePrefix = "/livesync/v1";
+const LiveShareRoutePrefix = "livesync/v1";
 const GetNtpTimeRoute = "getNTPTime";
 const GetFluidTenantInfoRoute = "fluid/tenantInfo/get";
 const RegisterClientRolesRoute = "clientRoles/register";
@@ -19,76 +17,147 @@ const FluidContainerSetRoute = "fluid/container/set";
 
 export class MockServerLiveShareHost implements ILiveShareHost {
   private constructor(
-    private readonly axios: AxiosInstance,
+    private readonly acsToken: string,
     private readonly meetingJoinUrl: string,
-    private readonly skypeMri: string
+    private readonly skypeMri: string,
+    private readonly apiEndpoint: string
   ) {}
+
+  public static create(options: LiveShareHostOptions): ILiveShareHost {
+    return new MockServerLiveShareHost(
+      options.userToken,
+      options.meetingJoinUrl,
+      options.skypeMri,
+      options.apiEndpoint
+    );
+  }
+
   async getClientRoles(
     clientId: string
   ): Promise<UserMeetingRole[] | undefined> {
     const request = this.constructBaseRequest() as FluidClientRolesInput;
     request.clientId = clientId;
-    const response = await this.axios.post<UserMeetingRole[]>(
-      `${LiveShareRoutePrefix}/${ClientRolesGetRoute}`,
-      request
+    const response = await fetch(
+      `${this.apiEndpoint}/${LiveShareRoutePrefix}/${ClientRolesGetRoute}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.acsToken}`,
+        },
+        body: JSON.stringify(request),
+      }
     );
-    return response.data;
+    const data = await response.json();
+    return data.roles;
   }
+
   async getFluidContainerId(): Promise<IFluidContainerInfo> {
     const request = this.constructBaseRequest() as FluidGetContainerIdInput;
-    const response = await this.axios.post<IFluidContainerInfo>(
-      `${LiveShareRoutePrefix}/${FluidContainerGetRoute}`,
-      request
+    const response = await fetch(
+      `${this.apiEndpoint}/${LiveShareRoutePrefix}/${FluidContainerGetRoute}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.acsToken}`,
+        },
+        body: JSON.stringify(request),
+      }
     );
-    return response.data;
+    const data = await response.json();
+    return data;
   }
+
   async getFluidTenantInfo(): Promise<IFluidTenantInfo> {
     const request = this.constructBaseRequest() as FluidTenantInfoInput;
     request.expiresAt = 0;
-    const response = await this.axios.post<FluidCollabTenantInfo>(
-      `${LiveShareRoutePrefix}/${GetFluidTenantInfoRoute}`,
-      request
+    const response = await fetch(
+      `${this.apiEndpoint}/${LiveShareRoutePrefix}/${GetFluidTenantInfoRoute}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.acsToken}`,
+        },
+        body: JSON.stringify(request),
+      }
     );
-    return response.data.broadcaster.frsTenantInfo;
+
+    const data = await response.json();
+    return data.broadcaster.frsTenantInfo;
   }
+
   async getFluidToken(containerId?: string): Promise<string> {
     const request = this.constructBaseRequest() as FluidGetTokenInput;
     request.containerId = containerId;
-    const response = await this.axios.post<any>(
-      `${LiveShareRoutePrefix}/${FluidTokenGetRoute}`,
-      request
+    const response = await fetch(
+      `${this.apiEndpoint}/${LiveShareRoutePrefix}/${FluidTokenGetRoute}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.acsToken}`,
+        },
+        body: JSON.stringify(request),
+      }
     );
-    return response.data.token;
+    const data = await response.json();
+    return data.token;
   }
+
   async getNtpTime(): Promise<INtpTimeInfo> {
-    const response = await this.axios.get<INtpTimeInfo>(
-      `${LiveShareRoutePrefix}/${GetNtpTimeRoute}`
+    const response = await fetch(
+      `${this.apiEndpoint}/${LiveShareRoutePrefix}/${GetNtpTimeRoute}`,
+      {
+        method: "GET",
+      }
     );
-    return response.data;
+    const data = await response.json();
+    return data;
   }
+
   async registerClientId(clientId: string): Promise<UserMeetingRole[]> {
     const request = this.constructBaseRequest() as FluidClientRolesInput;
     request.clientId = clientId;
-    const response = await this.axios.post<UserMeetingRole[]>(
-      `${LiveShareRoutePrefix}/${RegisterClientRolesRoute}`,
-      request
+    const response = await fetch(
+      `${this.apiEndpoint}/${LiveShareRoutePrefix}/${RegisterClientRolesRoute}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.acsToken}`,
+        },
+        body: JSON.stringify(request),
+      }
     );
-    return response.data;
+    const data = await response.json();
+    return data;
   }
+
   async setFluidContainerId(containerId: string): Promise<IFluidContainerInfo> {
     const request = this.constructBaseRequest() as FluidSetContainerIdInput;
     request.containerId = containerId;
-    const response = await this.axios.post(
-      `${LiveShareRoutePrefix}/${FluidContainerSetRoute}`,
-      request
+    const response = await fetch(
+      `${this.apiEndpoint}/${LiveShareRoutePrefix}/${FluidContainerSetRoute}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${this.acsToken}`,
+        },
+        body: JSON.stringify(request),
+      }
     );
-    return response.data;
+    const data = await response.json();
+    return data;
   }
+
   private constructBaseRequest(): LiveShareRequestBase {
     const originUri = window.location.href;
     return {
-      appId: "test",
       originUri,
+      appId: "com.stickynotes.test",
       teamsContextType: TeamsCollabContextType.MeetingJoinUrl,
       teamsContext: {
         meetingJoinUrl: this.meetingJoinUrl,
@@ -96,44 +165,29 @@ export class MockServerLiveShareHost implements ILiveShareHost {
       },
     };
   }
-  public static create(options: LiveShareHostOptions): ILiveShareHost {
-    const axiosDefaults: CreateAxiosDefaults<any> = {
-      baseURL: options.apiEndpoint,
-    };
-    const axiosConfig = { ...axiosDefaults, ...options?.axiosOptions };
-    const axiosInstance = axios.create(axiosConfig);
-    axiosInstance.interceptors.request.use((config) => {
-      const { userToken } = options;
-      config.headers.set("Authorization", `Bearer ${userToken}`, true);
-      return config;
-    });
-    return new MockServerLiveShareHost(
-      axiosInstance,
-      options.meetingJoinUrl,
-      options.skypeMri
-    );
-  }
 }
-export interface LiveShareHostOptions {
-  apiEndpoint: string;
-  meetingJoinUrl: string;
-  skypeMri: string;
-  userToken: string;
-  axiosOptions?: CreateAxiosDefaults<any>;
+
+export interface AcsLiveShareHostOptions {
+  teamsMeetingJoinUrl: string;
+  acsTokenProvider: () => string;
 }
+
 interface FluidCollabTenantInfo {
   broadcaster: BroadcasterInfo;
 }
+
 interface BroadcasterInfo {
   type: string;
   frsTenantInfo: FluidTenantInfo;
 }
+
 interface FluidTenantInfo {
   tenantId: string;
   ordererEndpoint: string;
   storageEndpoint: string;
   serviceEndpoint: string;
 }
+
 interface FluidTenantInfoInput {
   appId?: string;
   originUri: string;
@@ -141,23 +195,29 @@ interface FluidTenantInfoInput {
   teamsContext: TeamsContext;
   expiresAt: number;
 }
+
 interface FluidGetContainerIdInput extends LiveShareRequestBase {}
+
 interface TeamsContext {
   meetingJoinUrl?: string;
   skypeMri?: string;
 }
+
 interface FluidSetContainerIdInput extends LiveShareRequestBase {
   containerId: string;
 }
+
 interface FluidContainerInfo {
   containerState: ContainerState;
   shouldCreate: boolean;
   containerId: string;
   retryAfter: number;
 }
+
 interface FluidClientRolesInput extends LiveShareRequestBase {
   clientId: string;
 }
+
 interface FluidGetTokenInput {
   appId?: string;
   originUri: string;
@@ -166,16 +226,26 @@ interface FluidGetTokenInput {
   containerId?: string;
   // TODO: these are not used on server side    // userId?: string;    // userName?: string;
 }
+
 interface User {
   mri: string;
 }
+
 enum TeamsCollabContextType {
   MeetingJoinUrl = 1,
   GroupChatId,
 }
+
 interface LiveShareRequestBase {
   appId?: string;
   originUri: string;
   teamsContextType: TeamsCollabContextType;
   teamsContext: TeamsContext;
+}
+
+export interface LiveShareHostOptions {
+  apiEndpoint: string;
+  meetingJoinUrl: string;
+  skypeMri: string;
+  userToken: string;
 }
